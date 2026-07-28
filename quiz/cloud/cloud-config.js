358c6f3316b959e6dfed008796ec2585c810f527
tantkde180504 <tantkde180504@fpt.edu.vn>
2026-07-28 22:59:09 +0700

    Switch to personal Neon database

--- quiz/cloud/cloud-config.js
+++ quiz/cloud/cloud-config.js
@@ -5,7 +5,7 @@
 window.STUDY_CLOUD = {
   MASTER_CODE: "TanTran@2004",
   DATABASE_URL:
-    "postgresql://neondb_owner:npg_FbhBZ6kQqL9P@ep-frosty-fire-auhn11sa-pooler.c-10.us-east-1.aws.neon.tech/neondb?sslmode=require",
+    "postgresql://neondb_owner:npg_wC42QfcGMugh@ep-autumn-sea-axan0fv8-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 };
 
 // Alias for older MLN code paths
